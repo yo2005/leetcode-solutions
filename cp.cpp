18604126fe173746ea1e2d374a33d479e1c37be8
yo2005 <ohywhy9005@gmail.com>
2026-01-20 20:15:46 +0530

    Update cp.cpp

--- cp.cpp
+++ cp.cpp
@@ -1,30 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<long long>a(n);
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        long long ans=-1;
-        for(long long x=2;x<=50;x++){
-            for(int i=0;i<n;i++){
-                if(gcd(a[i],x)==1){
-                    ans=x;
-                    break;
-                }
-            }
-            if(ans!=-1){
-                break;
-            }
-        }
-        cout<<ans<<endl;
-
-
-    }
-    return 0;
+    bool b=1;
 }
