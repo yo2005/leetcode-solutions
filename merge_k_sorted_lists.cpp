4deb6110c827a02cdc63b13c5aa2627427cefa68
Gauri S <gaurisonawane0001@gmail.com>
2026-03-03 00:25:06 +0530

    Create merge_k_sorted_lists.cpp

---
+++ merge_k_sorted_lists.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int,ListNode*>,vector<pair<int, ListNode*>>, greater<pair<int,ListNode*>>>pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]){
+                pq.push({lists[i]->val,lists[i]});
+
+            }
+        }
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+        while(!pq.empty()){
+            auto  it=pq.top();
+            pq.pop();
+            temp->next=it.second;
+            temp=temp->next;
+            if(it.second->next){
+                pq.push({it.second->next->val,it.second->next});
+            }
+
+        }
+       return dummy->next; 
+    }
+};
