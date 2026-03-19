3cb7172c842803c593d8565151c017d84e8604a5
Gauri S <gaurisonawane0001@gmail.com>
2026-03-19 17:05:07 +0530

    Create binary_tree_right_side_view.cpp

---
+++ binary_tree_right_side_view.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        if(root==NULL){
+            return res;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* z=q.front();
+                q.pop();
+                if(i==n-1){
+                    res.push_back(z->val);
+                }
+                if(z->left){
+                    q.push(z->left);
+                }
+                if(z->right){
+                    q.push(z->right);
+                }
+            }
+        }
+        return res;
+        
+    }
+};
