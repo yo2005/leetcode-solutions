421a53f49743fea0b4780324b8ba0e996d5cc58f
Gauri S <gaurisonawane0001@gmail.com>
2026-04-07 22:41:52 +0530

    Create implement_trie.cpp

---
+++ implement_trie.cpp
@@ -0,0 +1,73 @@
+struct Node{
+    Node* links[26];
+    bool flag=false;
+    bool ck(char ch){
+        return (links[ch-'a']!=NULL);
+    }
+     void put(char ch,Node* node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    bool setend(){
+        return flag=true;
+    }
+    bool isend(){
+        return flag;
+    }
+    };
+
+class Trie {
+    private:Node* root;
+public:
+    Trie() {
+        root=new Node();
+        
+    }
+   
+    
+    void insert(string word) {
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->ck(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+        }
+        node->setend();
+        
+    }
+    
+    bool search(string word) {
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->ck(word[i])){
+                return false;
+            }
+            node=node->get(word[i]);
+        }
+        return node->isend();
+        
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for(int i=0;i<prefix.length();i++){
+            if(!node->ck(prefix[i])){
+                return false;
+            }
+            node=node->get(prefix[i]);
+        }
+        return true;
+        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
